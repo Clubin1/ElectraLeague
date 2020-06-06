230b1cae844894a3c97a9b1eac3a3ae39d3dca78
Jean Tubay <jean.tubay.92@gmail.com>
2020-06-06 15:17:03 -0400

    adding sql query

--- app.js
+++ app.js
@@ -288,10 +288,10 @@ app.post('/user', (req, res) => {
     console.log(`${req.body.name}`)
     db.query('SELECT * FROM rankme WHERE name = ?', [req.body.name], (err, res) => {
         if(err) throw err
-        console.log(`${res[0].score}<====================`)
+        console.log(`${res[0].deaths}<====================`)
         console.log(req.session.username)
-        db.query('UPDATE registered_users SET rankme_name = ? WHERE name = ?', [res[0].name, req.session.username], (err, res) => {
-            console.log('ehyyy')
+        db.query('UPDATE registered_users SET rankme_name = ?, deaths = ? WHERE name = ?', [res[0].name, res[0].deaths, req.session.username], (err, result) => {
+            console.log('hey')
         })
        
     } )
